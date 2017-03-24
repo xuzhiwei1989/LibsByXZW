668d046bf5494efd4ab3deeae45c9dbc5e320030
xuzhiwei1989 <1351033315@qq.com>
2017-03-24 13:51:13 +0800

    .NET日志工具
    基于Apache log4net封装的日志工具，附配置文件

---
+++ XZW.Logs/LogLevel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XZW.Logs
+{
+    public enum LogLevel
+    {
+        All,
+        /// <summary>
+        /// 调试信息
+        /// </summary>
+        Debug,
+        /// <summary>
+        /// 一般信息
+        /// </summary>
+        Info,
+        /// <summary>
+        /// 警告
+        /// </summary>
+        Warn,
+        /// <summary>
+        /// 一般错误
+        /// </summary>
+        Error,
+        /// <summary>
+        /// 致命错误
+        /// </summary>
+        Fatal,
+        OFF
+    }
+}
---
+++ XZW.Logs/LogUtils.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using log4net;
+using log4net.Config;
+
+namespace XZW.Logs
+{
+    public static class LogUtils
+    {
+        public delegate void LogWriteEvent(LogLevel level, string msg);
+
+        public static string ConfigFile { get; set; }
+
+        private static ILog log;
+
+        private static LogWriteEvent logWriteEvent;
+        public static event LogWriteEvent WriteEvent
+        {
+            add
+            {
+                LogWriteEvent logWriteEvent = LogUtils.logWriteEvent;
+                LogWriteEvent logWriteEvent2;
+                do
+                {
+                    logWriteEvent2 = logWriteEvent;
+                    LogWriteEvent value2 = (LogWriteEvent)Delegate.Combine(logWriteEvent2, value);
+                    logWriteEvent = Interlocked.CompareExchange<LogWriteEvent>(ref LogUtils.logWriteEvent, value2, logWriteEvent2);
+                }
+                while (logWriteEvent != logWriteEvent2);
+            }
+            remove
+            {
+                LogWriteEvent logWriteEvent = LogUtils.logWriteEvent;
+                LogWriteEvent logWriteEvent2;
+                do
+                {
+                    logWriteEvent2 = logWriteEvent;
+                    LogWriteEvent value2 = (LogWriteEvent)Delegate.Remove(logWriteEvent2, value);
+                    logWriteEvent = Interlocked.CompareExchange<LogWriteEvent>(ref LogUtils.logWriteEvent, value2, logWriteEvent2);
+                }
+                while (logWriteEvent != logWriteEvent2);
+            }
+        }
+
+        public static ILog Default
+        {
+            get
+            {
+                if (LogUtils.log == null)
+                {
+                    if (File.Exists(LogUtils.ConfigFile))
+                    {
+                        XmlConfigurator.ConfigureAndWatch(new FileInfo(LogUtils.ConfigFile));
+                    }
+                    LogUtils.log = LogManager.GetLogger(typeof(LogUtils));
+                }
+                return LogUtils.log;
+            }
+        }
+
+        static LogUtils()
+        {
+            LogUtils.log = null;
+            string text = ConfigurationManager.AppSettings["XZW.Logs"];
+            if (string.IsNullOrEmpty(text))
+            {
+                text = "XZW.Logs.config";
+            }
+            if (!File.Exists(text) && !text.Contains("\\"))
+            {
+                text = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, text);
+            }
+            LogUtils.ConfigFile = text;
+        }
+
+        private static void WriteLog(LogLevel level, string msg, bool isTriggerWriteEvent)
+        {
+            if (isTriggerWriteEvent && LogUtils.logWriteEvent != null)
+            {
+                try
+                {
+                    LogUtils.logWriteEvent(level, msg);
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        public static void Debug(string msg, bool isTriggerWriteEvent = true)
+        {
+            LogUtils.Default.Debug(msg);
+            LogUtils.WriteLog(LogLevel.Debug, msg, isTriggerWriteEvent);
+        }
+
+        public static void Debug(string msg, Exception ex, bool isTriggerWriteEvent = true)
+        {
+            LogUtils.Debug(msg + ex.Message + ex.StackTrace, isTriggerWriteEvent);
+        }
+
+        public static void Info(string msg, bool isTriggerWriteEvent = true)
+        {
+            LogUtils.Default.Info(msg);
+            LogUtils.WriteLog(LogLevel.Info, msg, isTriggerWriteEvent);
+        }
+
+        public static void Info(string msg, Exception ex, bool isTriggerWriteEvent = true)
+        {
+            LogUtils.Info(msg + ex.Message + ex.StackTrace, isTriggerWriteEvent);
+        }
+
+        public static void Warn(string msg, bool isTriggerWriteEvent = true)
+        {
+            LogUtils.Default.Warn(msg);
+            LogUtils.WriteLog(LogLevel.Warn, msg, isTriggerWriteEvent);
+        }
+
+        public static void Warn(string msg, Exception ex, bool isTriggerWriteEvent = true)
+        {
+            LogUtils.Warn(msg + ex.Message + ex.StackTrace, isTriggerWriteEvent);
+        }
+
+        public static void Error(string msg, bool isTriggerWriteEvent = true)
+        {
+            LogUtils.Default.Error(msg);
+            LogUtils.WriteLog(LogLevel.Error, msg, isTriggerWriteEvent);
+        }
+
+        public static void Error(string msg, Exception ex, bool isTriggerWriteEvent = true)
+        {
+            LogUtils.Error(msg + ex.Message + ex.StackTrace, isTriggerWriteEvent);
+        }
+
+        public static void Fatal(string msg, bool isTriggerWriteEvent = true)
+        {
+            LogUtils.Default.Fatal(msg);
+            LogUtils.WriteLog(LogLevel.Fatal, msg, isTriggerWriteEvent);
+        }
+
+        public static void Fatal(string msg, Exception ex, bool isTriggerWriteEvent = true)
+        {
+            LogUtils.Fatal(msg + ex.Message + ex.StackTrace, isTriggerWriteEvent);
+        }
+    }
+}
